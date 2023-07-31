e37282b1b7b042670c771f6fb1fbdc6b4bd869c0
kouchikota <kouchikota@kouchikotanoMacBook-Air.local>
2023-07-31 19:33:34 +0900

    add all fiels

--- force-app/main/default/lwc/test/test.js
+++ force-app/main/default/lwc/test/test.js
@@ -1,6 +1,7 @@
 // test.js
 
 import { LightningElement } from 'lwc';
+import MyCustomButton from './myCustomButton.css';
 
 export default class Test extends LightningElement {
   handleClick() {
---
+++ force-app/main/default/lwc/test2/test2.js
@@ -0,0 +1,10 @@
+import { LightningElement } from "lwc";
+
+import MyCustomButton from "./myCustomButton.css";
+
+export default class Test extends LightningElement {
+  handleClick() {
+    // ボタンがクリックされたときに実行されるロジック
+    alert("ボタンがクリックされました！");
+  }
+}
