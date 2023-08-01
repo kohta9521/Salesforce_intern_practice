1189d5ba51755f93c08dbc73f3adb15ac564331b
kouchikota <kouchikota@kouchikotanoMacBook-Air.local>
2023-08-01 16:01:07 +0900

    add new content about input tags but erroro not acces key for me

--- force-app/main/default/lwc/boxSample/boxSample.js
+++ force-app/main/default/lwc/boxSample/boxSample.js
@@ -1,3 +1,14 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from "lwc";
 
-export default class BoxSample extends LightningElement {}
+export default class BoxSample extends LightningElement {
+  @api recordId;
+  @api objectName;
+  @api fields;
+  @api title;
+  @api iconName;
+  @api mode;
+
+  connectedCallback() {
+    this.fields = this.fields ? this.fields.split(",") : [];
+  }
+}
